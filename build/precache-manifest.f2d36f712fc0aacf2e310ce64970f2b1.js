fc6699d308991eb920a2941c15d5937dadafe485
Frida <frida@thoughtbot.com>
2023-06-01 21:07:07 -0500

    update build

---
+++ build/precache-manifest.f2d36f712fc0aacf2e310ce64970f2b1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a9ae681ba6d4cbf099d",
+    "url": "/adopt-a-pup/static/js/main.8a9ae681.chunk.js"
+  },
+  {
+    "revision": "9d9234016002649642e9",
+    "url": "/adopt-a-pup/static/css/1.59c3f213.chunk.css"
+  },
+  {
+    "revision": "9d9234016002649642e9",
+    "url": "/adopt-a-pup/static/js/1.9d923401.chunk.js"
+  },
+  {
+    "revision": "250d0757c5bc9dc5a37a",
+    "url": "/adopt-a-pup/static/js/runtime~main.250d0757.js"
+  },
+  {
+    "revision": "e27271a7000d883275637477bb85e756",
+    "url": "/adopt-a-pup/static/media/eltonMain.e27271a7.png"
+  },
+  {
+    "revision": "604916d5c8a0ee5f4a7b96b2bf036a08",
+    "url": "/adopt-a-pup/static/media/elton1.604916d5.jpg"
+  },
+  {
+    "revision": "d4edc4992b3871409a145844b243ab41",
+    "url": "/adopt-a-pup/static/media/elton2.d4edc499.jpg"
+  },
+  {
+    "revision": "09e2866d92cfa7db1be3fa083801812e",
+    "url": "/adopt-a-pup/static/media/elton3.09e2866d.jpg"
+  },
+  {
+    "revision": "5141beb5b10375e53b0124ce46b7cdc8",
+    "url": "/adopt-a-pup/static/media/eltonVid.5141beb5.mp4"
+  },
+  {
+    "revision": "f74d1b8aa0f86cfd1adde23a6eff661e",
+    "url": "/adopt-a-pup/static/media/eltonVid2.f74d1b8a.mp4"
+  },
+  {
+    "revision": "c8f81721f14cd7d6075e86273f83c338",
+    "url": "/adopt-a-pup/index.html"
+  }
+];
